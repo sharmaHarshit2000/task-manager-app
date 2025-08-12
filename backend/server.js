c1e1a11c7ef968170f0838250d24ef0ee2920e62
sharmaHarshit2000 <harshitsharma9989@gmail.com>
2025-08-12 07:37:57 +0530

    add cors middleware

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import cors from "cors";
 import authRoutes from "./routes/auth.js";
 import taskRoutes from "./routes/tasks.js";
 
 dotenv.config();
 const app = express();
 app.use(express.json());
+app.use(cors())
 
 app.use("/api/auth", authRoutes);
 app.use("/api/tasks", taskRoutes);
