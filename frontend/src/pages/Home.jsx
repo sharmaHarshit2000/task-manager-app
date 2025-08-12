730ee5301fe600d203591ff076de16d3fe332f72
sharmaHarshit2000 <harshitsharma9989@gmail.com>
2025-08-12 08:48:40 +0530

    pages addded

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import Navbar from "../components/Navbar";
+import TaskList from "../components/Tasks/TaskList";
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <TaskList />
+    </>
+  );
+}
